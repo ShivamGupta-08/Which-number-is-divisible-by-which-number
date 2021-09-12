c64812f572b9f08202398041ba843d19080f3353
Shivam Gupta <87187728+ShivamGupta-08@users.noreply.github.com>
2021-09-12 12:08:52 +0530

    Update Which-number-is-divisible-by-which-number.py

--- Which-number-is-divisible-by-which-number.py
+++ Which-number-is-divisible-by-which-number.py
@@ -1,14 +1,18 @@
-print("Enter the number of apples harry have")
-apples = int(input())
-print("Enter mn numbers")
-mn = int(input())
-print("Enter mx numbers")
-mx = int(input())
-x = range(mn, mx+1)
-for n in x:
-    div = apples/n
-    if mn==mx:
-        print("This is not a range, and mn is equal to mx")
-    if str(div).endswith(".0"):
-        print(f"{n} is a divisor of {apples}")
-    else:print(f"{n} is  not a divisor of {apples}")
+try:
+    print("Enter the number of apples harry have")
+    apples = int(input())
+    print("Enter mn numbers")
+    mn = int(input())
+    print("Enter mx numbers")
+    mx = int(input())
+    if mn >= mx:
+        print("This is not a range, because mn is greater than mx")
+        exit()
+    for n in range(mn, mx+1):
+        div = apples/n
+        if mn<mx:
+            if str(div).endswith(".0"):
+                print(f"{n} is a divisor of {apples}")
+            else:print(f"{n} is  not a divisor of {apples}")
+except ValueError as e:
+    print("Only numbers",e)
